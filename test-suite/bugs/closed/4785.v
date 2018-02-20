4189bb08f39c56881d3483ef4bcca2f6c845f53a
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-02-20 16:03:57 +0100

    Remove 8.5 compatibility support.

--- test-suite/bugs/closed/4785.v
+++ test-suite/bugs/closed/4785.v
@@ -1,5 +1,4 @@
 Require Coq.Lists.List Coq.Vectors.Vector.
-Require Coq.Compat.Coq85.
 
 Module A.
 Import Coq.Lists.List Coq.Vectors.Vector.
@@ -21,12 +20,11 @@ Delimit Scope mylist_scope with mylist.
 Bind Scope mylist_scope with mylist.
 Arguments mynil {_}, _.
 Arguments mycons {_} _ _.
-Notation " [] " := mynil (compat "8.5") : mylist_scope.
+Notation " [] " := mynil (compat "8.6") : mylist_scope.
 Notation " [ ] " := mynil (format "[ ]") : mylist_scope.
 Notation " [ x ] " := (mycons x nil) : mylist_scope.
 Notation " [ x ; y ; .. ; z ] " :=  (mycons x (mycons y .. (mycons z nil) ..)) : mylist_scope.
 
-Import Coq.Compat.Coq85.
 Locate Module VectorNotations.
 Import VectorDef.VectorNotations.
 
@@ -37,7 +35,6 @@ Check [ ]%list : list _.
 End A.
 
 Module B.
-Import Coq.Compat.Coq85.
 
 Goal True.
   idtac; []. (* Check that importing the compat file doesn't break the [ | .. | ] syntax of Ltac *)
