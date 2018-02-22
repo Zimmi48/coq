41f38f16654c674c73f57acbaf23033c58782df7
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2018-02-22 22:11:23 +0100

    Overlay for HoTT.

---
+++ dev/ci/user-overlays/06791-Zimmi48-remove-compat-8-5.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "6791" ] || [ "$CI_BRANCH" = "remove/compat/8.5" ]; then
+    HoTT_CI_BRANCH=compat-with-coq-pr-6791
+    HoTT_CI_GITURL=https://github.com/Zimmi48/HoTT.git
+fi
